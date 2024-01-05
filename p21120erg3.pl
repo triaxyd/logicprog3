8bd6274b618f45351223f6d4bda46fe658ec601e
Triantafyllos Xydis <triantafyllosxyd@gmail.com>
2024-01-05 20:57:51 +0200

    final commit

--- p21120erg3.pl
+++ p21120erg3.pl
@@ -3,23 +3,23 @@
 :- discontiguous automata_numbers/2.
 :- discontiguous automata_text/2.
 :- discontiguous automata/2.
-:- discontiguous automata_max_moves/3.
+:- discontiguous automata_max_moves/4.
 
 
-% number states %
+% number states 
 state(s0).
 state(s1).
 state(s2).
 state(s3).
 final_state(sf).
 
-% text states (some redundant)%
+% text states (some redundant) 
 state(s1).
 state(s2).
 final_state(s3).
 state(s4).
 
-% numbers transitions%
+% numbers transitions 
 trans(s0,5,s1).
 trans(s0,10,s2).
 trans(s0,20,sf).
@@ -39,90 +39,97 @@ trans(s3,null,s1).
 trans(s3,b,s4).
 
 
-% helper checking for no integer transition
-check_number(Transition):-
-	number(Transition).
-% check if all transitions are numbers
-check_numbers(Transitions):-
-	maplist(number,Transitions).
+% check if X is number
+check_number(X):-
+	number(X).
+% check for number to all elements on list
+check_numbers(X):-
+	maplist(number,X).
 
-% helper to check if transition not number 
-check_not_number(Transition) :-
-    \+ number(Transition).
+% check if X is not a number
+check_not_number(X) :-
+    \+ number(X).
 
-% check if all transitions are not numbers
-check_not_numbers(Transitions) :-
-    maplist(check_not_number, Transitions).
+% check if all elements on list are not numbers
+check_not_numbers(X) :-
+    maplist(check_not_number, X).
 
 % check for epsilon transitions and get the next state
 check_for_epsilon(State,NextState):-
 	trans(State,null,NextState).
 
+% decrement number by 1
 decr(Num,DecrNum) :- DecrNum is Num-1.
 
-% automata/2 base cases -> we are at sf and list is empty
+% base cases for each category of automata -> final state and empty list
 automata(sf,[]).
 automata(s3,[]).
 automata_numbers(sf,[]).
 automata_text(s3,[]).
-automata_max_moves(s3,[],_).
+automata_max_moves(s3,[],_,_).
 
 
 % automata/2 body for numbers
 automata(State, [Transition|TransitionList]) :-
+	nonvar(Transition),
+	nonvar(TransitionList),
     check_number(Transition),
     check_numbers(TransitionList),
     automata_numbers(State,[Transition|TransitionList]).
 
 % automata/2 body for not numbers
 automata(State, [Transition|TransitionList]) :-
+	nonvar(Transition),
+	nonvar(TransitionList),
     check_not_number(Transition),
     check_not_numbers(TransitionList),
     automata_text(State,[Transition|TransitionList]).
 	
 
-% automata numbers case %
+% automata_numbers/2 numbers case 
 automata_numbers(State,[Transition|TransitionList]):-
 	trans(State,Transition,NextState),
 	automata_numbers(NextState,TransitionList).
 
 
-% automata text case 
+% automata_text/2 text case 
 automata_text(State,[Transition|TransitionList]):-
 	trans(State,Transition,NextState),
 	automata_text(NextState,TransitionList).
 
-% check for epsilon - null transitions - only for text
+% automata_text/2 text case check for epsilon transitions
 automata_text(State,[Transition|TransitionList]):-
 	check_for_epsilon(State,EpsilonNext),
-	trans(EpsilonNext,Transition,NextState),
-	automata_text(NextState,TransitionList).
+	nonvar(EpsilonNext),
+	automata_text(EpsilonNext,[Transition|TransitionList]).
 
 
-% check for circles with automata/3 - implemented for text only since numbers dont have circles
+% automata/3 check for circles on text automata 
 automata(State,[Transition|TransitionList],Max_Moves):-
 	nonvar(Transition),
 	nonvar(TransitionList),
 	nonvar(Max_Moves),
 	number(Max_Moves),
-	automata_max_moves(State,[Transition|TransitionList],Max_Moves).
+	automata_max_moves(State,[Transition|TransitionList],Max_Moves,Max_Moves).
 
+% automata_max_moves/3 case for circles, decrement moves by 1 
+automata_max_moves(State,[Transition|TransitionList],Max_Moves,Moves):-
+	trans(State,Transition,State),
+	decr(Moves,MovesLeft),
+	MovesLeft>=0,
+	automata_max_moves(State,TransitionList,Max_Moves,MovesLeft).
 
-% automata case for circles - no epsilon
-automata_max_moves(State,[Transition|TransitionList],Moves):-
+ % automata_max_moves/3 case for non-circles, reset moves to max no circle found 
+automata_max_moves(State,[Transition|TransitionList],Max_Moves,_):-
 	trans(State,Transition,NextState),
-	decr(Moves,MovesLeft),
-	MovesLeft>=0,
-	automata_max_moves(NextState,TransitionList,MovesLeft).
+	State \= NextState,
+	automata_max_moves(NextState,TransitionList,Max_Moves,Max_Moves). 
 	
-% automata case for circles - with epsilon transitions
-automata_max_moves(State,[Transition|TransitionList],Moves):-
+% automata_max_moves/3 case for epsilon, check next state if found
+automata_max_moves(State,[Transition|TransitionList],Max_Moves,Moves):-
 	check_for_epsilon(State,EpsilonNext),
-	trans(EpsilonNext,Transition,NextState),
-	decr(Moves,MovesLeft),
-	MovesLeft>=0,
-	automata_max_moves(NextState,TransitionList,MovesLeft).
-
+	nonvar(EpsilonNext),
+	automata_max_moves(EpsilonNext,[Transition|TransitionList],Max_Moves,Moves).
 
 
 
